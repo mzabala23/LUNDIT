74ada319e4b4e742e141870b3160a580861a36bb
cromero1908 <carlosfromerom@gmail.com>
2024-03-30 20:20:03 -0500

    Cambio de logos de pagina, fondo de pantalla y algunso datos de pagina

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -18,7 +18,7 @@ export const Navigation = (props) => {
             <span className="icon-bar"></span>{" "}
           </button>
           <a className="navbar-brand page-scroll" href="#page-top">
-            React Landing Page
+          <img src="img/logo-no-background.svg" className="img-home" alt="" />{" "}
           </a>{" "}
         </div>
 
